3e4db1bea0148bd963b774b8dfde40284cd749a7
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-10 12:59:59 +0530

    WPF-55603 - sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.CellLongPressed += AssociatedObject_CellLongPressed;
+        }
+        private void AssociatedObject_CellLongPressed(object sender, CellLongPressedEventArgs e)
+        {
+            MessageBox.Show("DateCell" + " " + e.DateTime.Day + " " + "has been long pressed", "DateCellHold Response", MessageBoxButton.OK);
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.CellLongPressed -= AssociatedObject_CellLongPressed;
+            this.scheduler = null;
+        }
+    }
+}
+    
+
